ae9693286f58a86cbdeaaaa683760232f0436d46
NiK <nikrusk@list.ru>
2025-05-06 18:41:10 +0500

    With added comments

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,25 +1,25 @@
 package ru.netology.stats;
 
 public class StatsService {
-    public long allSalesSum(int[] sale) { //сумма всех продаж (использую long, т.к. сумма может быть до 5 млрд, что превышает значение типов int)
-        long sum = 0;
-        for (long purchase : sale) {
+    public long allSalesSum(int[] sale) { //сумма всех продаж (1. использую long, т.к. сумма может быть до 5 млрд, что превышает значение типов int, 2. параметр с типом массив)
+        long sum = 0; //сумма продаж более 5 млрд
+        for (int purchase : sale) { // целое число, поэтому int, не превышает 5 млрд
             sum += purchase;
         }
         return sum;
     }
 
-    public double saleAverageMonth(int[] sale) { // средняя сумма продаж в месяц (использую double, т.к. считаем среднюю сумму и получаем не целое число)
-       int numberSale = sale.length;
+    public double saleAverageMonth(int[] sale) { // средняя сумма продаж в месяц (1. использую double, т.к. считаем среднюю сумму и получаем не целое число, 2. параметр с типом массив)
+       int numberSale = sale.length // (int, т.к. целое число, не превышает 5 млрд)
         long sum = allSalesSum(sale); //(использую long, т.к. это сумма)
-        double averageMonth = sum / numberSale;
+        double averageMonth = sum / numberSale; //считаем среднюю, поэтому double, не целое число
         return averageMonth;
     }
 
-    public int maxSale(int[] sale) { // месяц продаж на максимальную сумму (используем int, т.к. идет расчет месяца, число не превышает 2 млрд., все остальные случаи так же)
-        int maxMonth = sale[0];
+    public int maxSale(int[] sale) { // месяц продаж на максимальную сумму (1. используем int, т.к. идет расчет месяца, число не превышает 2 млрд., все остальные случаи так же, 2. параметр с типом массив)
+        int maxMonth = sale[0]; //месяц целое число
 
-        for (int i = 0; i < sale.length; i++) {
+        for (int i = 0; i < sale.length; i++) { //целое число до 2 млрд
             if (sale[i] >= sale[maxMonth]) {
                 maxMonth = i;
             }
@@ -27,7 +27,7 @@ public class StatsService {
         return maxMonth + 1;
     }
 
-    public int minSales(int[] sales) { //месяц продаж на минимальную сумму
+    public int minSales(int[] sales) { //месяц продаж на минимальную сумму (то же самое, считаем месяц, где будет использоваться целое число до 2 млрд, во втором случае массив)
         int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
 
         for (int i = 0; i < sales.length; i++) {
@@ -39,11 +39,11 @@ public class StatsService {
         return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
     }
 
-    public int saleBelowAverage(int[] sale) { // месяц, где продажи ниже среднего
-        int averageSum = (int) saleAverageMonth(sale);
-        int numberBelowAverage = 0;
+    public int saleBelowAverage(int[] sale) { // месяц, где продажи ниже среднего (месяц, целое число до 2 млрд, поэтому int, во втором случае массив)
+        double averageSum = (double) saleAverageMonth(sale); //double в обоих случаях, т.к. считаем среднюю, ответ скорее всего не целое число
+        int numberBelowAverage = 0; //целое число до 2 млрд
 
-        for (int i = 0; i < sale.length; i++) {
+        for (int i = 0; i < sale.length; i++) { //целое число до 2 млрд
             if (sale[i] < averageSum) {
                 numberBelowAverage = numberBelowAverage + 1;
             }
@@ -51,11 +51,11 @@ public class StatsService {
         return numberBelowAverage;
     }
 
-    public int saleAboveAverage(int[] sale) { // месяц, где продажи выше среднего
-        int averageSum = (int) saleAverageMonth(sale);
-        int numberAboveAverage = 0;
+    public int saleAboveAverage(int[] sale) { // месяц, где продажи выше среднего (месяц, целое число до 2 млрд, поэтому int, во втором случае массив)
+        double averageSum = (double) saleAverageMonth(sale); //double в обоих случаях, т.к. считаем среднюю, ответ скорее всего не целое число
+        int numberAboveAverage = 0; //целое число до 2 млрд
 
-        for (int i = 0; i < sale.length; i++) {
+        for (int i = 0; i < sale.length; i++) {//целое число до 2 млрд
             if (sale[i] > averageSum) {
                 numberAboveAverage = numberAboveAverage + 1;
             }
