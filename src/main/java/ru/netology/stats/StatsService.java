a4913e252ade1cd304e09e97c33191bdd3d2698b
NiK <nikrusk@list.ru>
2025-05-07 11:40:55 +0500

    Add all comments

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,22 +1,22 @@
 package ru.netology.stats;
 
 public class StatsService {
-    public long allSalesSum(long[] sale) { //сумма всех продаж (1. использую long, т.к. сумма может быть свыше 2 млрд, целое число, 2. параметр с типом массив может быть свыше 2 млрд, целое число)
-        long sum = 0; //сумма продаж свыше 2 млрд
-        for (long purchase : sale) { // целое число, поэтому int, не превышает 2 млрд
+    public long allSalesSum(long[] sale) { // 1) сумма может быть свыше 2 млрд, целое число, 2) параметр с типом массив может быть свыше 2 млрд, целое число
+        long sum = 0; //сумма продаж может быть свыше 2 млрд, целое число
+        for (long purchase : sale) { //целое число, может превышать 2 млрд
             sum += purchase;
         }
         return sum;
     }
 
-    public long saleAverageMonth(long[] sale) { // средняя сумма продаж в месяц (1. сумма может быть свыше 2 млрд, целое число, 2. параметр с типом массив может быть свыше 2 млрд, целое число)
-        int numberSale = sale.length; // (int, т.к. целое число, не превышает 2 млрд)
-        long sum = allSalesSum(sale); //(использую long, т.к. это сумма может быть свыше 2 млрд, целое число)
+    public long saleAverageMonth(long[] sale) { // 1) сумма может быть свыше 2 млрд, целое число, 2) параметр с типом массив может быть свыше 2 млрд, целое число
+        int numberSale = sale.length; // не превышает 2 млрд, целое число
+        long sum = allSalesSum(sale); //сумма может быть свыше 2 млрд, целое число
         long averageMonth = sum / numberSale; //сумма может быть свыше 2 млрд, целое число
         return averageMonth;
     }
 
-    public int maxSale(long[] sale) { // месяц продаж на максимальную сумму (1. используем int, т.к. идет расчет месяца, число не превышает 2 млрд., , 2. параметр с типом массив не превышает 2 млрд, целое число)
+    public int maxSale(long[] sale) { //1) не превышает 2 млрд, целое число, 2) параметр с типом массив может быть свыше 2 млрд, целое число
         int maxMonth = 0; //месяц целое число, не превышает 2 млрд
 
         for (int i = 0; i < sale.length; i++) { //целое число до 2 млрд
@@ -27,20 +27,20 @@ public class StatsService {
         return maxMonth + 1;
     }
 
-    public int minSales(long[] sales) { //месяц продаж на минимальную сумму (то же самое, считаем месяц, где будет использоваться целое число до 2 млрд, во втором случае массив может превышать 2 млрд)
-        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее (месяц до 2 млрд, целое число)
+    public int minSales(long[] sales) { // 1) не превышает 2 млрд, целое число, 2) параметр с типом массив может быть свыше 2 млрд, целое число
+        int minMonth = 0; // месяц не превышает 2 млрд, целое число
 
-        for (int i = 0; i < sales.length; i++) {
-            if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
-                minMonth = i; // запомним его как минимальный
+        for (int i = 0; i < sales.length; i++) { //целое число до 2 млрд
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
             }
         }
 
-        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+        return minMonth + 1;
     }
 
-    public int saleBelowAverage(long[] sale) { // месяц, где продажи ниже среднего (месяц, целое число до 2 млрд, целое число, поэтому int, во втором случае массив может превышать 2 млрд)
-        int averageSum = (int) saleAverageMonth(sale); //double в обоих случаях, т.к. считаем среднюю, ответ скорее всего не целое число
+    public int saleBelowAverage(long[] sale) { // 1) месяц, целое число до 2 млрд, целое число 2) параметр с типом массив может быть свыше 2 млрд, целое число
+        int averageSum = (int) saleAverageMonth(sale); //целое число до 2 млрд в обоих случаях
         int numberBelowAverage = 0; //целое число до 2 млрд
 
         for (int i = 0; i < sale.length; i++) { //целое число до 2 млрд
@@ -51,7 +51,7 @@ public class StatsService {
         return numberBelowAverage;
     }
 
-    public int saleAboveAverage(long[] sale) { // месяц, где продажи выше среднего (месяц, целое число до 2 млрд, поэтому int, во втором случае массив может превышать 2 млрд)
+    public int saleAboveAverage(long[] sale) { // 1) месяц, целое число до 2 млрд, целое число 2) параметр с типом массив может быть свыше 2 млрд, целое число
         int averageSum = (int) saleAverageMonth(sale); //целое число до 2 млрд
         int numberAboveAverage = 0; //целое число до 2 млрд
 
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -8,9 +8,9 @@ public class StatsServiceTest {
     @Test
     public void allSalesSum() {
         StatsService service = new StatsService();
-        long[] sale = new long[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        long actual = service.allSalesSum(sale);
-        long expected = 180;
+        long[] sale = new long[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18}; //продажи могут быть свыше 2 млрд, целое числоЮ поэтому long в обеих случаях
+        long actual = service.allSalesSum(sale); //актуальное значение может быть больше 2 млрд, целое число
+        long expected = 180; //ожидаемое значение может быть больше 2 млр, целое число
         assertEquals(expected, actual);
     }
 
@@ -18,45 +18,45 @@ public class StatsServiceTest {
     public void saleAverageMonth() {
         StatsService service = new StatsService();
 
-        long[] sale = new long[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        long actual = service.saleAverageMonth(sale);
-        long expected = 15;
+        long[] sale = new long[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18}; //продажи могут быть свыше 2 млрд, целое числоЮ поэтому long в обеих случаях
+        long actual = service.saleAverageMonth(sale); //актуальное значение может быть больше 2 млрд, целое число
+        long expected = 15; //ожидаемое значение может быть больше 2 млр, целое число
         assertEquals(expected, actual);
     }
 
     @Test
     public void maxSale() {
         StatsService service = new StatsService();
-        long[] sale = new long[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int actual = service.maxSale(sale);
-        int expected = 8;
+        long[] sale = new long[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18}; //продажи могут быть свыше 2 млрд, целое числоЮ поэтому long в обеих случаях
+        int actual = service.maxSale(sale); // актуальное значение до 2 млрд, целое число
+        int expected = 8; //ожидаемо значение до 2 млрд, целое число
         assertEquals(expected, actual);
     }
 
     @Test
     public void minSales() {
         StatsService service = new StatsService();
-        long[] sale = new long[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int actual = service.minSales(sale);
-        int expected = 9;
+        long[] sale = new long[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18}; //продажи могут быть свыше 2 млрд, целое числоЮ поэтому long в обеих случаях
+        int actual = service.minSales(sale); // актуальное значение до 2 млрд, целое число
+        int expected = 9; //ожидаемо значение до 2 млрд, целое число
         assertEquals(expected, actual);
     }
 
     @Test
     public void saleBelowAverage() {
         StatsService service = new StatsService();
-        long[] sale = new long[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int actual = service.saleBelowAverage(sale);
-        int expected = 5;
+        long[] sale = new long[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18}; //продажи могут быть свыше 2 млрд, целое числоЮ поэтому long в обеих случаях
+        int actual = service.saleBelowAverage(sale); // актуальное значение до 2 млрд, целое число
+        int expected = 5; //ожидаемо значение до 2 млрд, целое число
         assertEquals(expected, actual);
     }
 
     @Test
     public void saleAboveAverage() {
         StatsService service = new StatsService();
-        long[] sale = new long[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int actual = service.saleAboveAverage(sale);
-        int expected = 5;
+        long[] sale = new long[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18}; //продажи могут быть свыше 2 млрд, целое числоЮ поэтому long в обеих случаях
+        int actual = service.saleAboveAverage(sale); // актуальное значение до 2 млрд, целое число
+        int expected = 5; //ожидаемо значение до 2 млрд, целое число
         assertEquals(expected, actual);
     }
 }
